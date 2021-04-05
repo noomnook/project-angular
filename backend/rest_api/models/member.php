32ce8f56cb73cf3aa485d86e4307878891dd72a1
noomnook <noompakonnoo@gmail.com>
2021-04-05 16:25:31 +0700

    test

--- backend/rest_api/models/member.php
+++ backend/rest_api/models/member.php
@@ -118,7 +118,7 @@ class Member
             }
             return $data;
         } catch (PDOException $e) {
-            echo "Error: " . $e->getMessage();
+            echo "Error2: " . $e->getMessage();
         }
     }
 }
